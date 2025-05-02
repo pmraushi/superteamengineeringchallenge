75faeded05168ef4e31f32479dd21fe8e6019968
pmraushi <philemonandrew20@gmail.com>
2025-05-03 00:56:43 +0300

    feat: user login

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -17,5 +17,13 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 
 
 @router.post("/login")
-def login():
-    return {"message": "User logged-in successful"}
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = get_user_by_username(db, username=form_data.username)
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
