623e8d6ea831b2a3e6f425dbee25385d8a7035ae
pmraushi <philemonandrew20@gmail.com>
2025-05-03 03:45:38 +0300

    project complete - end time: 2025-05-03 03:45 EAT

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -10,6 +10,7 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 def override_get_db():
     try:
         db = TestingSessionLocal()
